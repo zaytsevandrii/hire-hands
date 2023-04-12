20b51fec95a790369a8235971c7a7e10c73f2b69
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-04-12 14:14:21 +0100

    add transform menu with all components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,63 @@
-import React from 'react'
+import React, { useState } from "react"
 import styles from "../styles/Navbar.module.scss"
+import Link from "next/link"
+import Image from "next/image"
+import logo from "../public/logo.png"
+import Hamburger from "./Hamburger"
 
 const Navbar = () => {
-  return (
-    <div className={styles.navbar} ref={modalRef}>
+    const [open, setOpen] = useState(false)
+    const closeModal = () => {}
+    const openModal = () => {
+        setOpen((prev) => !prev)
+    }
+    return (
+        <>
+            <div className={`${open && "overlay-show"}`}></div>
+            <div id="mobile-menu" className={`mobile-main-menu ${open && "show-menu"}`}>
+                <ul>
+                    <Link href="/goods/collections">
+                        <li onClick={closeModal}>Серебро</li>
+                    </Link>
+
+                    <Link href="/goods/bijouterie">
+                        <li onClick={closeModal}>Бижутерия </li>
+                    </Link>
+
+                    <Link href="/goods/watch">
+                        <li onClick={closeModal}>Часы </li>
+                    </Link>
+
+                    <Link href="/goods/bags">
+                        <li onClick={closeModal}>Сумки</li>
+                    </Link>
+                    <Link href="/goods/children-jewelry">
+                        <li onClick={closeModal}>Детские украшения</li>
+                    </Link>
+                    <Link href="/goods/scarves">
+                        <li onClick={closeModal}>Шарфы</li>
+                    </Link>
+                    <Link href="/goods/perfumery">
+                        <li onClick={closeModal}>Парфюмерия</li>
+                    </Link>
+                    <Link href="/goods/natural-stones">
+                        <li onClick={closeModal}>Натуральные камни</li>
+                    </Link>
+                    <Link href="/goods/new-product">
+                        <li onClick={closeModal}>Новинки</li>
+                    </Link>
+                </ul>
+                {/* <div className="mobile-menu-info">
+                    © 2023
+                    <span style={{ fontSize: "18px" }}> 925Kazakhstan</span>
+                </div> */}
+            </div>
+            <div className={styles.navbar} /* ref={modalRef} */>
                 <div className="container py-1">
                     <div className="col-sm-12  ">
                         <div className="row">
                             <div className="col-4  d-flex align-items-center">
-                                <div className={styles.left}>
-                                    <Link href="/address" style={{ color: "#fff" }}>
-                                        <div className={styles.map}>
-                                            <Image src="/loc.svg" alt="search" width={23} height={23} />
-                                            <div>
-                                                Наши <br />
-                                                пунткты выдачи
-                                            </div>
-                                        </div>
-                                    </Link>
-                                    <div className={styles.phone}>
-                                        <Image src="/phone.svg" alt="search" width={23} height={23} />
-                                        <div>
-                                            8 775 623 49 63 <br />
-                                            горячая линия
-                                        </div>
-                                    </div>
-                                </div>
+                                <div className={styles.left}></div>
                                 <div className={styles.leftH}>
                                     <Hamburger open={open} openModal={openModal} />
                                 </div>
@@ -33,108 +65,23 @@ const Navbar = () => {
                             <div className="col-4  d-flex align-items-center  justify-content-center">
                                 <div className={styles.center}>
                                     <Link href="/">
-                                        <Image src={logo} priority width={130} alt="logo" onClick={closeModal} />
+                                        <Image src={logo} priority width={300} alt="logo" onClick={closeModal} />
                                     </Link>
                                 </div>
                             </div>
                             <div className="col-4  d-flex align-items-center justify-content-end">
                                 <div className={styles.right}>
-                                    <Image
-                                        src="/search.svg"
-                                        alt="search"
-                                        width={23}
-                                        height={23}
-                                        className={styles.img1}
-                                        onClick={handleSearchClick}
-                                    />
                                     <div className={styles.user}>
-                                        {status === "loading" ? (
-                                            <Image
-                                                src="/user.svg"
-                                                alt="user"
-                                                width={28}
-                                                height={28}
-                                                className={styles.img2}
-                                                onClick={handleLoginClick}
-                                            />
-                                        ) : session?.user ? (
-                                            <div className={styles.userName}>
-                                                <DropDownLink
-                                                    className={styles.dropdown}
-                                                    userName={session.user.name}
-                                                    logoutClickHandler={logoutClickHandler}
-                                                    session={session}
-                                                />
-                                                {/* {session.user.name.charAt(0).toUpperCase()} */}
-                                            </div>
-                                        ) : (
-                                            <Image
-                                                src="/user.svg"
-                                                alt="user"
-                                                width={28}
-                                                height={28}
-                                                className={styles.img2}
-                                                onClick={handleLoginClick}
-                                            />
-                                        )}
-                                    </div>
-                                    <div className={styles.cart}>
-                                        <Link href="/basket" alt="корзина" onClick={closeModal}>
-                                            {
-                                                /* cart.cartItems.length */ cartItemsCount > 0 && (
-                                                    <p>
-                                                        {cartItemsCount}
-                                                        {/* {cart.cartItems.reduce((a, c) => a + c.quantity, 0)} */}
-                                                    </p>
-                                                )
-                                            }
-
-                                            <Image src="/cart.svg" alt="cart" width={23} height={23} className={styles.img3} />
-                                        </Link>
+                                        <Image src="/user.svg" alt="user" width={28} height={28} className={styles.img2} />
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={styles.navbar2}>
-                    <div className="container  d-flex justify-content-around">
-                        <Link href="/goods/collections">
-                            <li>Серебро</li>
-                        </Link>
-                        <Link href="/goods/bijouterie">
-                            <li>Бижутерия</li>
-                        </Link>
-                        <Link href="/goods/watch">
-                            <li>Часы</li>
-                        </Link>
-                        <Link href="/goods/bags">
-                            <li>Сумки</li>
-                        </Link>
-                        <Link href="/goods/children-jewelry">
-                            <li>Детские украшения</li>
-                        </Link>
-                        <Link href="/goods/scarves">
-                            <li>Шарфы</li>
-                        </Link>
-                        <Link href="/goods/perfumery">
-                            <li>Парфюмерия</li>
-                        </Link>
-                        <Link href="/goods/natural-stones">
-                            <li>Натуральные камни</li>
-                        </Link>
-                        <Link href="/goods/new-product">
-                            <li>Новинки</li>
-                        </Link>
-                        <Link href="/goods/checkpage">
-                            <li>Check</li>
-                        </Link>
-                    </div>
-                </div>
-                <LoginModal show={showLoginModal} handleClose={handleLoginClose} allClose={allClose} />
-                <SearchModal show={showSearch} handleClose={handleSearchClose} allClose={allClose} />
             </div>
-  )
+        </>
+    )
 }
 
 export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from "../components/Layout"
+import "../styles/globals.scss"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
 
 export default MyApp
